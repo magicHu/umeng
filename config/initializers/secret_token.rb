29fb19757b95d119b1d00830ffbb03111eb7e70a
MagicHu <huronghai2008@gmail.com>
2011-12-14 21:30:51 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Umeng::Application.config.secret_token = '6fe9329150f38cabdff541c28ec5d695ed8b3fee45bff504ee2ef0ac1ffaae52556ad1df14eaae29b951f6ade3ffd098d2826d9d8092d8dc9251c4593e722194'
